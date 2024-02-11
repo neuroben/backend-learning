bd8071d826bc0638f4fa658cff5b0075479bc164
Bence A Kovacs <bencearon@gmail.com>
2024-02-11 22:36:05 +0100

    added rich text editor

--- pages/newpost/index.jsx
+++ pages/newpost/index.jsx
@@ -1,23 +1,40 @@
 import * as React from 'react';
+import { useState } from 'react';
+import { EditorState, convertToRaw } from 'draft-js';
+import draftToHtml from 'draftjs-to-html';
 import Head from 'next/head';
-import '../../src/app/styles/global.css';
-import Button from '../../src/app/components/button/Button';
-import SideNav from '@/app/components/nav/sideNav';
-import Box from '@mui/material/Box';
+import dynamic from 'next/dynamic';
 import Layout from '@/app/components/layout/Layout';
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+import "./AddPost.css";
 
+const Editor = dynamic(
+  () => import('react-draft-wysiwyg').then(module => module.Editor),
+  { ssr: false }
+);
 
-function LandingPage() {
+function AddPost() {
+    const [editorState, setEditorState] = useState(() => EditorState.createEmpty());
+
+    const handleEditorChange = (state) => {
+        setEditorState(state);
+
+        const rawContentState = convertToRaw(editorState.getCurrentContent());
+        const markup = draftToHtml(rawContentState);
+        console.log(markup);
+    };
+
     return (
-        <>
-            <Head>
-            </Head>
-            
-            <Layout>
-            
-            </Layout>
-        </>
+        <Layout>
+            <Editor
+                editorState={editorState}
+                toolbarClassName="toolbarClassName"
+                wrapperClassName="wrapperClassName"
+                editorClassName="editorClassName"
+                onEditorStateChange={handleEditorChange}
+            />
+        </Layout>
     );
 }
 
-export default LandingPage;
+export default AddPost;
